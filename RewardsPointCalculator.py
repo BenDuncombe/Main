9e839fe51619bc4c60b6d1566dea978054098211
BenDuncombe <79779533+BenDuncombe@users.noreply.github.com>
2021-02-28 14:00:54 +0800

    Add files via upload

---
+++ RewardsPointCalculator.py
@@ -0,0 +1,14 @@
+a = 0
+b = 100
+c = 101
+d = 500
+e = 501
+points_earned = int(input("enter the total value of your shopping: "))
+if points_earned >= a and points_earned <= b:
+    print("congratulations 10 points")
+elif points_earned >= c and points_earned <= d:
+    print("congratulations 20 points")
+elif points_earned > e:
+    print("congratulations 40 points")
+                     
+                     
---
+++ middlenumber.py
@@ -0,0 +1,4 @@
+A = int(input("enter your first number ..."))
+B = int(input("enter your second number ..."))
+C = int(input("enter your third number ..."))
+print("Your middle number is",B)
